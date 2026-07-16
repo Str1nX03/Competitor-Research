a3b1b329ac6cdb44f7ad8b36f6ebb3674c2825fd
Str1nX03 <draveen2210@gmail.com>
2026-07-16 14:21:03 +0530

    past reports can be deleted now

--- src/prompts/reporter_prompt.py
+++ src/prompts/reporter_prompt.py
@@ -3,6 +3,8 @@ REPORT_GENERATOR_PROMPT = """
 You are a very good business analyst and a report writer.
 Write a very good and comprehensive report showcasing as much data as you can based on the research data provided:-
 
+Company Name: {name}
+Research Data:
 {research_data}
 
 This should be the template you should follow to lay down the report:-
